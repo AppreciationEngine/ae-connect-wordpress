332772d62fe3a62d16c4af7f8361cd1999e0f38a
avesk <averykushner@gmail.com>
2018-08-20 14:58:13 -0700

    - functional for ae-link - values aren't saved

--- admin/js/ae-connect-shortcode-generator.js
+++ admin/js/ae-connect-shortcode-generator.js
@@ -5,11 +5,14 @@
         const aeWindowAdder = jQuery("#ae-window button");
         const aeOnPageAdder = jQuery("#ae-on-page button");
         const aeLogoutAdder = jQuery("#ae-logout button");
+        const generate = jQuery(".ae-generate");
 
         aeLinkAdder.click({type: "link"}, addGenerator);
         aeWindowAdder.click({type: "window"}, addGenerator);
         aeOnPageAdder.click({type: "on-page"}, addGenerator);
         aeLogoutAdder.click({type: "logout"}, addGenerator);
+
+        generate.click(generateShortcode);
 
     });
 
@@ -24,4 +27,55 @@ function addGenerator(event) {
     if(originalLink.css('display') == 'none') {
         originalLink.show();
     }
+}
+
+function generateShortcode(event) {
+    // var btn = jQuery(event.target);
+    const shortCodeConfig = jQuery(event.target).parent(".shortcode-config-inner");
+    // console.log(shortCodeConfig);
+    var data = {};
+    shortCodeConfig.children('p').each(function () {
+        let input = this.querySelector('input');
+        let select = this.querySelector('select');
+        if(input) {
+            data[input.name] = input.value;
+            // console.log(input.name);
+            // console.log(input.value);
+        }
+        if(select) {
+            data[select.name] = select.value;
+            // console.log(select.name);
+            // console.log(select.value);
+        }
+    });
+    console.log(data);
+
+    setShortcodeText(shortCodeConfig, data);
+
+}
+
+function setShortcodeText(shortCodeConfig, data) {
+    var endshortcode, text, shortcode;
+    endshortcode = text = shortcode = "";
+
+    if('text' in data) {
+        text = data.text;
+        delete data.text;
+    }
+
+    if('endshortcode' in data) {
+        endshortcode = data.endshortcode;
+        delete data.endshortcode;
+    }
+
+    shortcode = data.shortcode;
+    delete data.shortcode;
+
+    var args = "";
+    for (var key in data) {
+        args += key + '="' + data[key] + '" ';
+    }
+    var shortcode = "["+shortcode+" "+args+"]"+text+endshortcode;
+    shortCodeConfig.find("p.shortcode-rendered").html(shortcode);
+    console.log(shortcode);
 }
