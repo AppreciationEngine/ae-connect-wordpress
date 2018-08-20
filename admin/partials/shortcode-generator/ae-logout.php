32c3a1a2841b5303d1af593ccde2c94137e8c0d8
avesk <averykushner@gmail.com>
2018-08-20 15:48:16 -0700

    - can add and remove ae logout link generators - Does not save state

--- admin/partials/shortcode-generator/ae-logout.php
+++ admin/partials/shortcode-generator/ae-logout.php
@@ -1,15 +1,22 @@
 <div class="shortcode-config">
+    <a class="remove-generator" href="#">X</a>
     <h3>AE LOGOUT LINK</h3>
     <div class="shortcode-config-inner">
+        <p style="display: none;">
+            <input name="shortcode" type="hidden" value="ae-logout">
+        </p>
+        <p style="display: none;">
+            <input name="endshortcode" type="hidden" value="[/ae-logout]">
+        </p>
         <p>
             <label for="text">Link Text</label>
-            <input class="widefat" name="text" type="text" value="Sign In">
+            <input class="widefat" name="text" type="text" value="Logout">
         </p>
         <p>
             <label for="text">Return URL</label>
             <input class="widefat" name="return" type="text">
         </p>
-        <label for="type">Your Shortcode!</label>
-        <p class="shortcode-rendered">[ae-form]</p>
+        <button class="button button-primary ae-generate">Submit</button>
+        <p class="shortcode-rendered">[ae-logout]Logout[/ae-logout]</p>
     </div>
 </div>
