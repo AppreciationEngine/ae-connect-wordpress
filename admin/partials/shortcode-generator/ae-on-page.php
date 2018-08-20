c80838194e34a7095d5f438e8acfe83fbe61b082
avesk <averykushner@gmail.com>
2018-08-20 15:44:36 -0700

    - can add and remove ae-form generators

--- admin/partials/shortcode-generator/ae-on-page.php
+++ admin/partials/shortcode-generator/ae-on-page.php
@@ -1,4 +1,5 @@
 <div class="shortcode-config">
+    <a class="remove-generator" href="#">X</a>
     <h3>AE ON PAGE FORM</h3>
     <div class="shortcode-config-inner">
         <label for="type">Your Shortcode!</label>
